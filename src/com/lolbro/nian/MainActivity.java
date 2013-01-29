a698b5656ceff4b0d59d2dfac57cef2843154b3c
drewi <andre.kindwall@gmail.com>
2013-01-29 21:12:22 +0100

    Adjustments

--- src/com/lolbro/nian/MainActivity.java
+++ src/com/lolbro/nian/MainActivity.java
@@ -3,9 +3,16 @@ package com.lolbro.nian;
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.extension.physics.box2d.FixedStepPhysicsWorld;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+import com.badlogic.gdx.math.Vector2;
 
 public class MainActivity extends SimpleBaseGameActivity {
 	
@@ -13,14 +20,20 @@ public class MainActivity extends SimpleBaseGameActivity {
 	// Constants
 	// ===========================================================
 	
-	private static final int CAMERA_WIDTH = 720;
-	private static final int CAMERA_HEIGHT = 480;
+	private static final int CAMERA_WIDTH = 480;
+	private static final int CAMERA_HEIGHT = 720;
+	
+	private static final int STEPS_PER_SECOND = 30;
 	
 	// ===========================================================
 	// Fields
 	// ===========================================================
 	
 	private Camera mCamera;
+	
+	private Scene mScene;
+	
+	private PhysicsWorld mPhysicsWorld;
 	
 	// ===========================================================
 	// Constructors
@@ -35,23 +48,34 @@ public class MainActivity extends SimpleBaseGameActivity {
 	// ===========================================================
 	
 	@Override
-	public EngineOptions onCreateEngineOptions() {
+	public EngineOptions onCreateEngineOptions() {		
 		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-
-		return new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera);
+		
+		//new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT)
+		return new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new FillResolutionPolicy(), this.mCamera);
 	}
 	
 	@Override
 	protected void onCreateResources() {
-		// TODO Auto-generated method stub
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
 		
 	}
 	
 	@Override
 	protected Scene onCreateScene() {
-		// TODO Auto-generated method stub
-		return null;
+		this.mScene = new Scene();
+		this.mScene.setBackground(new Background(21, 37, 67));
+		
+		this.mPhysicsWorld = new FixedStepPhysicsWorld(STEPS_PER_SECOND, new Vector2(0, 0), false, 8, 1);
+		
+		this.mScene.registerUpdateHandler(this.mPhysicsWorld);
+		
+		return this.mScene;
 	}
+	
+	// ===========================================================
+	// Methods
+	// ===========================================================
 	
 	// ===========================================================
 	// Inner and Anonymous Classes
