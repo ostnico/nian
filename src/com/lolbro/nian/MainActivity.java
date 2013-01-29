fac10d780e309d97faf89bec65b25ec4945fd27a
drewi <andre.kindwall@gmail.com>
2013-01-29 20:45:43 +0100

    Removed unnecessary files

--- src/com/lolbro/nian/MainActivity.java
+++ src/com/lolbro/nian/MainActivity.java
@@ -1,22 +1,60 @@
 package com.lolbro.nian;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SimpleBaseGameActivity {
+	
+	// ===========================================================
+	// Constants
+	// ===========================================================
+	
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
+	
+	// ===========================================================
+	// Fields
+	// ===========================================================
+	
+	private Camera mCamera;
+	
+	// ===========================================================
+	// Constructors
+	// ===========================================================
 
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+	public EngineOptions onCreateEngineOptions() {
+		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
 
+		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera);
+	}
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
+	protected void onCreateResources() {
+		// TODO Auto-generated method stub
+		
 	}
-
+	
+	@Override
+	protected Scene onCreateScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+	
 }
